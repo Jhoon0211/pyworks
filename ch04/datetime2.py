6ee8a3c06076e1e9555e71de67ce11a77e56e85b
Jhoon0211 <ajh1070337@naver.com>
2023-04-20 18:18:28 +0900

    datetime

--- ch04/datetime2.py
+++ ch04/datetime2.py
@@ -1,6 +1,10 @@
 # 나이가 100세가 되는 해의 년도 계산하기
 # 현재년도 + (100 - age)
 import datetime
+import calendar as cal
+
+cal.prcal(2023) # 2023년 달력
+cal.prcal(2023, 4) # 2023년 4월 달력
 
 age = int(input("나이 입력 : "))
 now = datetime.date.today()
